277ee20b6bf3b090ff9ee6dd9a21992270f632a5
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-30 20:06:31 +0100

    using default arguments and return keyword to add values to blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,19 @@
 # Defining our blockchain as a empty list
-blockchain = [[1]]
+blockchain = []
 
 
-# Build Function to add values to the blockchain
-def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
+# Function to return latest blockchain value
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+# Function to add values to the blockchain
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
 
 # Invoke Function
 add_value(2)
-add_value(0.9)
-add_value(10.89)
+add_value(0.9, get_last_blockchain_value())
+add_value(10.89, get_last_blockchain_value())
