72a5a5e67efecd307095ef3720a9fc91bea6a700
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-30 20:25:45 +0100

    Using the build-in input function to let the user choose how the transaction amount

--- blockchain.py
+++ blockchain.py
@@ -10,10 +10,21 @@ def get_last_blockchain_value():
 # Function to add values to the blockchain
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
-    print(blockchain)
 
 
-# Invoke Function
-add_value(2)
-add_value(0.9, get_last_blockchain_value())
-add_value(10.89, get_last_blockchain_value())
+# Function to let the user input the amount of the transaction
+def get_user_input():
+    return float(input('Your transaction amount please: '))
+
+
+# Invoke Function with repetitive use of build-in input function
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+print(blockchain)
