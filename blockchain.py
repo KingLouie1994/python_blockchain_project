2490c9ec931b5e2f857347d53d168edcc61eee90
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-02 13:41:41 +0100

    Using conditions to handle different user inputs

--- blockchain.py
+++ blockchain.py
@@ -20,22 +20,49 @@ def add_value(transaction_amount, last_transaction=[1]):
 
 
 # Function to let the user input the amount of the transaction
-def get_user_input():
+def get_transaction_value():
     return float(input('Your transaction amount please: '))
 
 
-# Invoke get_user_input function for the first time
-tx_amount = get_user_input()
-add_value(tx_amount)
+# Function to receive users choices of inputs
+def get_user_choice():
+    user_input = input("Your choice: ")
+    return user_input
 
-# Using while loop to start an inifinite loop to add 'transactions' to the blockchain
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
 
-    # Output the blockchain to the console using for loop
+# Output the blockchain to the console using for loop
+def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+
+
+# Invoke get_user_input function to add a first value to the blockchain
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+
+while True:
+    """ Printing out the different input options """
+    print("Please choose!")
+    print("1: Add a new transaction value")
+    print("2: Output the blockchain values")
+    print("q: Quit")
+
+    """ Receiving users choice """
+    user_choice = get_user_choice()
+
+    """ Using conditions to invoke functions depending on users input """
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        print("Choice registered!")
+        break
+    else:
+        print("Please choose one of the existing options")
+    print("Choice registered!")
 
 print("Done!")
