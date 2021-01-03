90e0a608b20edf77ac2534ca3de24a25e7efa7c7
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-03 13:58:42 +0100

    added a get_balances function using list comprehension and if conditions

--- blockchain.py
+++ blockchain.py
@@ -10,9 +10,26 @@ owner = 'Luis'
 participants = {'Luis'}
 
 
-#
+# Reusabele function to create a hashed block
 def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
+
+
+# Function to receive balance of a participant of the blockchain
+def get_balances(participant):
+    tx_sender = [[tx['amount'] for tx in block['transactions']
+                  if tx['sender'] == participant] for block in blockchain]
+    amount_sent = 0
+    for tx in tx_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions']
+                     if tx['recipient'] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in tx_recipient:
+        if len(tx) > 0:
+            amount_received += tx[0]
+    return amount_received - amount_sent
 
 
 # Function to return latest blockchain value
@@ -52,6 +69,7 @@ def mine_block():
         'transactions': open_transactions
     }
     blockchain.append(block)
+    return True
 
 
 # Function to let the user input the amount of the transaction
@@ -110,7 +128,8 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
@@ -130,5 +149,6 @@ while waiting_for_input:
     if not verify_chain():
         print("Invalid blockchain")
         break
+    print(get_balances(owner))
 else:
     print("User left!")
