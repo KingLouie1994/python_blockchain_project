3af0e3b2bf40364e49eeada7d8aef2616d21597d
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-10 13:12:08 +0100

    created an unlimited_arguments function to learn how to use *

---
+++ functions.py
@@ -0,0 +1,8 @@
+def unlimited_arguments(*args, **keyword_args):
+    for argument in args:
+        print(argument)
+    for k, argument in keyword_args.items():
+        print(k, argument)
+
+
+unlimited_arguments(1, 2, 3, 4, name='Max', age=29)
