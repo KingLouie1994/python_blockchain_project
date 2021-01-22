ef24e7f642cfec2f16f901747897e55acbd27286
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-22 13:55:19 +0100

    blocks are transformed into data type that json can handle

--- Project/blockchain.py
+++ Project/blockchain.py
@@ -40,7 +40,7 @@ def load_data():
                     [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
                 updated_transactions.append(updated_transaction)
             open_transactions = updated_transactions
-    except IOError:
+    except (IOError, IndexError):
         genesis_block = Block(0, '', [], 100, 0)
         blockchain = [genesis_block]
         open_transactions = []
@@ -54,7 +54,8 @@ load_data()
 def save_data():
     try:
         with open('blockchain.txt', mode='w') as f:
-            f.write(json.dumps(blockchain))
+            savable_chain = [block.__dict__ for block in blockchain]
+            f.write(json.dumps(savable_chain))
             f.write('\n')
             f.write(json.dumps(open_transactions))
             # save_data = {
