a4ef1851ff3671f90998d557d8708eb675ef0fbb
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-22 12:10:06 +0100

    Private and public attributes

--- Examples/oop/car.py
+++ Examples/oop/car.py
@@ -3,11 +3,15 @@ class Car:
     # warnings = []
     def __init__(self, starting_top_speed=100):
         self.top_speed = starting_top_speed
-        self.warnings = []
+        self.__warnings = []
+
+    def add_warning(self, warning_text):
+        if len(warning_text) > 0:
+            self.__warnings.append(warning_text)
 
     def __repr__(self):
         print('Printing...')
-        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.warnings))
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.__warnings))
 
     def drive(self):
         print('I am driving but not faster than {}'.format(self.top_speed))
@@ -17,14 +21,14 @@ car1 = Car()
 car1.drive()
 
 # Car.top_speed = 200
-car1.warnings.append('New Warning!')
+car1.add_warning('New Warning!')
 # print(car1.__dict__)
 print(car1)
 
 car2 = Car(200)
 car2.drive()
-print(car2.warnings)
+# print(car2.__warnings)
 
 car3 = Car(250)
 car3.drive()
-print(car2.warnings)
+# print(car2.__warnings)
