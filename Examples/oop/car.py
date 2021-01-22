74644f444bcea792f0764e84763c895b88d2e20a
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-22 12:03:13 +0100

    using more dunder methods

--- Examples/oop/car.py
+++ Examples/oop/car.py
@@ -5,6 +5,10 @@ class Car:
         self.top_speed = starting_top_speed
         self.warnings = []
 
+    def __repr__(self):
+        print('Printing...')
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.warnings))
+
     def drive(self):
         print('I am driving but not faster than {}'.format(self.top_speed))
 
@@ -12,9 +16,10 @@ class Car:
 car1 = Car()
 car1.drive()
 
-#Car.top_speed = 200
+# Car.top_speed = 200
 car1.warnings.append('New Warning!')
-print(car1.warnings)
+# print(car1.__dict__)
+print(car1)
 
 car2 = Car(200)
 car2.drive()
@@ -23,4 +28,3 @@ print(car2.warnings)
 car3 = Car(250)
 car3.drive()
 print(car2.warnings)
-
