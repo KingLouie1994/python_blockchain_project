1d7ca1b2c225626545c7a1d90bf76eb1de985dda
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-28 08:00:55 +0100

    Broadcasting transactions prepared

--- Project/node.py
+++ Project/node.py
@@ -151,6 +151,40 @@ def broadcast_transaction():
         return jsonify(response), 500
 
 
+@app.route('/broadcast-block', methods=['POST'])
+def broadcast_block():
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': 'No data found.'
+        }
+        return jsonify(response), 400
+    if 'block' not in values:
+        response = {
+            'message': 'Some data is missing.'
+        }
+        return jsonify(response), 400
+    block = values['block']
+    if block['index'] == blockchain.chain[-1].index + 1:
+        if blockchain.add_block(block):
+            response = {
+                'message': 'Block added.'
+            }
+            return jsonify(response), 201
+        else:
+            response = {
+                'message': 'Block seems invalid.'
+            }
+            return jsonify(response), 500
+    elif block['index'] > blockchain.chain[-1].index:
+        pass
+    else:
+        response = {
+            'message': 'Blockchain seems to be shorter, block not added'
+        }
+        return jsonify(response), 409
+
+
 @app.route('/transactions', methods=['GET'])
 def get_open_transactions():
     transactions = blockchain.get_open_transactions()
